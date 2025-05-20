6fcf4537d091e56f5e00d2fe734cb516578bfa5a
Arjuna Pratama <ap5175856@gmail.com>
2025-05-20 11:03:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //  Lagu, Penyanyi, Likes, Play, link
 
 var lagu = [
-    ['Mangu' , 'Fourtwnty' , 14044 , 28023 , "mangu.jpeg"],
-    ['Right Now' , 'NewJeans' , 20501 , 25090 , "right-now.jpeg"],
-    ['Lesung pipi' , 'Raimlaode' , 18541 , 27062 , "lesung-pipi.jpeg"],
-    ['HANDS UP' , 'MEOVV' , 11099 , 20299 , "hands-up.jpeg"],
-    ['love.' , 'Wave to Earth' , 20129 , 33000 , "love..jpeg"],
-    ['Season of Memories' , 'Gfriend' , 12102 , 23402 , "season-of-memories.jpeg"],
-    ['See That?' , 'NMIXX' , 22002 , 28043 , "see-that.jpeg"],
-    ['Love Language' , 'Tomorrow X Together' , 21501 , 32398 , "love-language.jpeg"],
-    ['HOT' , 'Le Sserafim' , 23231 , 37062 , "hot.jpg"],
-    ['My Girl' , 'A.C.E' , 24649 , 32532 , "my-girl.jpeg"],
-    ["Car's Outside" , 'James Arthur' , 43719 , 53320 , "cars-outside.jpeg"],
-    ['Say Yes' , 'Loco,Punch' , 22102 , 32402 , "say-yes.jpeg"],
+    ['Mangu' , 'Fourtwnty' , 14044 , 28023 , "img/mangu.jpeg"],
+    ['Right Now' , 'NewJeans' , 20501 , 25090 , "img/right-now.jpeg"],
+    ['Lesung pipi' , 'Raimlaode' , 18541 , 27062 , "img/lesung-pipi.jpeg"],
+    ['HANDS UP' , 'MEOVV' , 11099 , 20299 , "img/hands-up.jpeg"],
+    ['love.' , 'Wave to Earth' , 20129 , 33000 , "img/love..jpeg"],
+    ['Season of Memories' , 'Gfriend' , 12102 , 23402 , "img/season-of-memories.jpeg"],
+    ['See That?' , 'NMIXX' , 22002 , 28043 , "img/see-that.jpeg"],
+    ['Love Language' , 'Tomorrow X Together' , 21501 , 32398 , "img.love-language.jpeg"],
+    ['HOT' , 'Le Sserafim' , 23231 , 37062 , "img.hot.jpg"],
+    ['My Girl' , 'A.C.E' , 24649 , 32532 , "img.my-girl.jpeg"],
+    ["Car's Outside" , 'James Arthur' , 43719 , 53320 , "img/cars-outside.jpeg"],
+    ['Say Yes' , 'Loco,Punch' , 22102 , 32402 , "img.say-yes.jpeg"],
 ];
 
 // variabel
